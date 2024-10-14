40d9160efc5c20b9598169b5c457d426bd48f12d
junjianni <junjian.ni@syngenta.com>
2024-10-13 21:51:36 -0400

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class UsersController(DataContext context) : BaseApiController
+{
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+        [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        return user;
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+var app = builder.Build();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.MapControllers();
+
+app.Run();
